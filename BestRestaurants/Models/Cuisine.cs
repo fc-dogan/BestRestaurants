69db442fb1effce9880594f4e90ec42acf3679e8
Fatma <fceyda@gmail.com>
2020-03-18 11:09:56 -0700

    add HomeController.cs and Home/Index.cshtml
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    Co-auatored-by: Jieun Kang <jieunkang101@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- BestRestaurants/Models/Cuisine.cs
+++ BestRestaurants/Models/Cuisine.cs
@@ -4,13 +4,14 @@ namespace BestRestaurants.Models
 {
   public class Cuisine
   {
-    public Cuisine()
-    {
-      this.Restaurants = new HashSet<Restaurant>();
-    }
 
     public int CuisineId { get; set; }
     public string Name { get; set; }
     public virtual ICollection<Restaurant> Restaurants { get; set; }
+    
+    public Cuisine()
+    {
+      this.Restaurants = new HashSet<Restaurant>();
+    }
   }
 }
