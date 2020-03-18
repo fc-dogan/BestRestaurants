7a06f38554a34b892b54cb5614a5cb62c67cdad1
Fatma <fceyda@gmail.com>
2020-03-18 11:54:28 -0700

    create RestaurantsController with index and create route
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    Co-auatored-by: Jieun Kang <jieunkang101@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(restaurants => restaurants.Cuisine).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // public ActionResult Details(int id)
+    // {
+    //   Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    //   return View(thisItem);
+    // }
+
+    // public ActionResult Edit(int id)
+    // {
+    //   var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    //   ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    //   return View(thisItem);
+    // }
+
+    // [HttpPost]
+    // public ActionResult Edit(Item item)
+    // {
+    //   _db.Entry(item).State = EntityState.Modified;
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+
+    // public ActionResult Delete(int id)
+    // {
+    //   var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    //   return View(thisItem);
+    // }
+
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    //   _db.Items.Remove(thisItem);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+  }
+}
