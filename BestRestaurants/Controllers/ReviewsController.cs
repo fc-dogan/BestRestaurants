3b86c19d2e6fca585fbbd917e026d6effd0f63d3
Fatma <fceyda@gmail.com>
2020-03-19 10:34:40 -0700

    create ReviewsController.cs
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jieun Kang <jieunkang101@gmail.com>

---
+++ BestRestaurants/Controllers/ReviewsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  public class ReviewsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public ReviewsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+  }
+
+
+}
