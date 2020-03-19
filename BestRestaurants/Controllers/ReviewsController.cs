a67fdc08fc064a7d80d15787a7b95e928e07018b
Fatma <fceyda@gmail.com>
2020-03-19 14:27:23 -0700

    create Review/ Edit.cshtml and update ReviewsControllers
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jieun Kang <jieunkang101@gmail.com>

--- BestRestaurants/Controllers/ReviewsController.cs
+++ BestRestaurants/Controllers/ReviewsController.cs
@@ -36,6 +36,40 @@ namespace BestRestaurants.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      Review thisReview = _db.Reviews.FirstOrDefault(reviews => reviews.ReviewId == id);
+      return View(thisReview);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisReview= _db.Reviews.FirstOrDefault(reviews => reviews.ReviewId == id);
+      ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
+      return View(thisReview);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Review review)
+    {
+      _db.Entry(review).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisReview = _db.Reviews.FirstOrDefault(reviews => reviews.ReviewId == id);
+      return View(thisReview);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisReview = _db.Reviews.FirstOrDefault(reviews => reviews.ReviewId == id);
+      _db.Reviews.Remove(thisReview);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 
